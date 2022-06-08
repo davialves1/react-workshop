fbbb7dee389aa55d87db9336620b474492f3e4ab
Davi Alves <extern.davi.alves@volkswagen.de>
2022-06-08 17:29:30 +0200

    changed to production endpoint

--- src/DockComponent.js
+++ src/DockComponent.js
@@ -14,7 +14,7 @@ const DockComponent = () => {
   const fetchData = async () => {
     setIsLoading(true);
     await axios
-      .get("http://localhost:8080/dataset")
+      .get("http://18.193.129.166")
       .catch((err) => console.error(err))
       .then((response) => setData(response.data.dataset));
     setIsLoading(false);
